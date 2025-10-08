cea22f39c4b290e9de568fcf958f86d82595621e
Dmitrii <bankersenator@gmail.com>
2025-10-08 03:55:11 +0000

    Optimize and add caching to KawaseBlurFilter
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- libs/renderengine/skia/filters/GaussianBlurFilter.cpp
+++ libs/renderengine/skia/filters/GaussianBlurFilter.cpp
@@ -47,7 +47,7 @@ GaussianBlurFilter::GaussianBlurFilter(RuntimeEffectManager& effectManager)
 
 sk_sp<SkImage> GaussianBlurFilter::generate(SkiaGpuContext* context, const uint32_t blurRadius,
                                             const sk_sp<SkImage> input,
-                                            const SkRect& blurRect) const {
+                                            const SkRect& blurRect) {
     // Create blur surface with the bit depth and colorspace of the original surface
     SkImageInfo scaledInfo = input->imageInfo().makeWH(std::ceil(blurRect.width() * kInputScale),
                                                        std::ceil(blurRect.height() * kInputScale));
